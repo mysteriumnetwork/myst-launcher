0c206aa50ff372ed9c78d582fe152e01f395e789
Anton <jnashicq@gmail.com>
2021-08-23 11:55:01 +0300

    Prevent infinite update to recent version

--- myst/repo.go
+++ myst/repo.go
@@ -26,7 +26,8 @@ func CheckVersionAndUpgrades(imageDigest string, c *model.Config) bool {
 			data, err = os.ReadFile(f)
 		}
 	}
-	if len(data) == 0 {
+
+	getFile := func() bool {
 		url := "https://registry.hub.docker.com/v2/repositories/mysteriumnetwork/myst/tags?page_size=30"
 		resp, err := http.Get(url)
 		if err != nil {
@@ -35,10 +36,18 @@ func CheckVersionAndUpgrades(imageDigest string, c *model.Config) bool {
 		if resp.StatusCode != 200 {
 			return false
 		}
-		data, _ := ioutil.ReadAll(resp.Body)
+		data, _ = ioutil.ReadAll(resp.Body)
 		os.WriteFile(f, data, 0777)
 
 		c.RefreshLastUpgradeCheck()
+		return true
+	}
+
+	if len(data) == 0 {
+		ok := getFile()
+		if !ok {
+			return false
+		}
 	}
 
 	// results
@@ -46,39 +55,51 @@ func CheckVersionAndUpgrades(imageDigest string, c *model.Config) bool {
 	latestVersion := ""
 	currentVersion := ""
 
-	jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		name, err := jsonparser.GetString(value, "name")
-		if err != nil {
-			return
-		}
-
-		jsonparser.ArrayEach(value, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-			digest, err := jsonparser.GetString(value, "digest")
+	parseJson := func() {
+		jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			name, err := jsonparser.GetString(value, "name")
 			if err != nil {
 				return
 			}
 
-			if name == imageTag {
-				latestDigest = digest
-			}
-			match := versionRegex.MatchString(name)
-			// a work-around for testnet3, b/c there's only 1 version of image
-			if imageTag == "testnet3" {
-				match = true
-			}
-			if match && latestDigest == digest {
-				latestVersion = name
-			}
-			digestsMatch := strings.ToLower(digest) == strings.ToLower(imageDigest)
-			if digestsMatch && match {
-				currentVersion = name
-			}
-		}, "images")
-	}, "results")
+			jsonparser.ArrayEach(value, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+				digest, err := jsonparser.GetString(value, "digest")
+				if err != nil {
+					return
+				}
+
+				if name == imageTag {
+					latestDigest = digest
+				}
+				match := versionRegex.MatchString(name)
+				// a work-around for testnet3, b/c there's only 1 version of image
+				if imageTag == "testnet3" {
+					match = true
+				}
+				if match && latestDigest == digest {
+					latestVersion = name
+				}
+				digestsMatch := strings.ToLower(digest) == strings.ToLower(imageDigest)
+				if digestsMatch && match {
+					currentVersion = name
+				}
+			}, "images")
+		}, "results")
+	}
+	parseJson()
 
 	gui.UI.HasUpdate = false
 	if (latestDigest != "" && imageDigest != "") && latestDigest != imageDigest {
-		gui.UI.HasUpdate = true
+		// re-try on fresh data
+		ok := getFile()
+		if !ok {
+			return false
+		}
+		parseJson()
+
+		if (latestDigest != "" && imageDigest != "") && latestDigest != imageDigest {
+			gui.UI.HasUpdate = true
+		}
 	}
 	gui.UI.VersionCurrent = currentVersion
 	gui.UI.VersionLatest = latestVersion
