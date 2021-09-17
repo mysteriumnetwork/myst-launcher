4dec96513c7acfa887c8f55aad72fc54f87f26f7
Anton <jnashicq@gmail.com>
2021-09-17 18:45:43 +0300

    Add docker download &install steps

--- app/install.go
+++ app/install.go
@@ -35,56 +35,61 @@ func (s *AppState) tryInstall() bool {
 		s.model.SwitchState(model.UIStateInstallError)
 		return true
 	}
-
+	s.model.UpdateProperties(model.UIProps{"CheckDocker": true})
 	if hasDocker {
-		s.model.UpdateProperties(model.UIProps{"CheckDocker": true})
-	} else {
-		//log.Println("Please install Docker Desktop")
-		//s.model.SwitchState(model.UIStateInstallError)
-		//return true
-
-		name := "Docker.dmg"
-		url, err := utils.GetDockerDesktopLink()
-		if err != nil {
-			log.Println("Couldn't get Docker Desktop link")
-			s.model.SwitchState(model.UIStateInstallError)
+		s.model.SwitchState(model.UIStateInstallFinished)
+		ok := s.ui.WaitDialogueComplete()
+		if !ok {
 			return true
 		}
-		log.Println("Downloading Docker desktop: ", url)
-		err = utils.DownloadFile(utils.GetTmpDir()+name, url, func(progress int) {
-			if progress%10 == 0 {
-				log.Println(fmt.Sprintf("%s - %d%%", name, progress))
-			}
-		})
-		if err != nil {
-			log.Println("Couldn't get Docker Desktop")
-			s.model.SwitchState(model.UIStateInstallError)
-			return true
-		}
-
-		var buf bytes.Buffer
-		res, err := utils.CmdRun(&buf, "/usr/sbin/diskutil", "unmount", "/Volumes/Docker")
-		fmt.Println("cmd", buf.String(), res, err)
-		buf.Reset()
-
-		res, err = utils.CmdRun(&buf, "/usr/bin/hdiutil", "attach", utils.GetTmpDir()+name)
-		fmt.Println("cmd", buf.String(), res, err)
-		buf.Reset()
-
-		// cp -R /Volumes/Docker/Docker.app /Applications
-		res, err = utils.CmdRun(&buf, "/bin/cp", "-pR", "/Volumes/Docker/Docker.app", "/Applications")
-		fmt.Println("cmd", buf.String(), res, err)
-		buf.Reset()
-
-		//  xattr -d -r com.apple.quarantine /Applications/Docker.app
-		res, err = utils.CmdRun(&buf, "/usr/bin/xattr", "-d", "-r", "com.apple.quarantine", "/Applications/Docker.app")
-		fmt.Println("cmd", buf.String(), res, err)
-		buf.Reset()
-
-		res, err = utils.CmdRun(&buf, "/usr/sbin/diskutil", "unmount", "/Volumes/Docker")
-		fmt.Println("cmd", buf.String(), res, err)
-		buf.Reset()
+		s.model.SwitchState(model.UIStateInitial)
+		return false
 	}
+
+	name := "Docker.dmg"
+	url, err := utils.GetDockerDesktopLink()
+	if err != nil {
+		log.Println("Couldn't get Docker Desktop link")
+		s.model.SwitchState(model.UIStateInstallError)
+		return true
+	}
+	log.Println("Downloading Docker desktop: ", url)
+	err = utils.DownloadFile(utils.GetTmpDir()+name, url, func(progress int) {
+		if progress%10 == 0 {
+			log.Println(fmt.Sprintf("%s - %d%%", name, progress))
+		}
+	})
+	if err != nil {
+		log.Println("Couldn't get Docker Desktop")
+		s.model.SwitchState(model.UIStateInstallError)
+		return true
+	}
+	s.model.UpdateProperties(model.UIProps{"DownloadFiles": true})
+
+	var buf bytes.Buffer
+	res, err := utils.CmdRun(&buf, "/usr/sbin/diskutil", "unmount", "/Volumes/Docker")
+	fmt.Println("cmd", buf.String(), res, err)
+	buf.Reset()
+
+	res, err = utils.CmdRun(&buf, "/usr/bin/hdiutil", "attach", utils.GetTmpDir()+name)
+	fmt.Println("cmd", buf.String(), res, err)
+	buf.Reset()
+
+	// cp -R /Volumes/Docker/Docker.app /Applications
+	res, err = utils.CmdRun(&buf, "/bin/cp", "-pR", "/Volumes/Docker/Docker.app", "/Applications")
+	fmt.Println("cmd", buf.String(), res, err)
+	buf.Reset()
+
+	//  xattr -d -r com.apple.quarantine /Applications/Docker.app
+	res, err = utils.CmdRun(&buf, "/usr/bin/xattr", "-d", "-r", "com.apple.quarantine", "/Applications/Docker.app")
+	fmt.Println("cmd", buf.String(), res, err)
+	buf.Reset()
+
+	res, err = utils.CmdRun(&buf, "/usr/sbin/diskutil", "unmount", "/Volumes/Docker")
+	fmt.Println("cmd", buf.String(), res, err)
+	buf.Reset()
+
+	s.model.UpdateProperties(model.UIProps{"InstallDocker": true})
 
 	s.model.SwitchState(model.UIStateInstallFinished)
 	ok := s.ui.WaitDialogueComplete()
