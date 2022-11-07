cce8b97a9a0fbc9a06f31ecd128489fe6daf677d
Anton Litvinov <jnashicq@gmail.com>
2022-11-08 02:20:58 +0400

    Build msi package

--- installer/installer.cmd
+++ installer/installer.cmd
@@ -3,8 +3,7 @@ set PATH=%PATH%;C:\Program Files (x86)\NirSoft\IconsExtract
 
 
 del installer\*.msi installer\*.wixobj installer\*.ico
+
 iconsext.exe /save "bin\myst-launcher-amd64.exe" "installer\" -icons
-
-
 candle installer\installer.wxs installer\licenseDialogue.wxs  -arch x64 -out installer\
 light installer\installer.wixobj installer\licenseDialogue.wixobj -dcl:high -ext WixUIExtension.dll -ext WixUtilExtension.dll -out installer\launcher-x64.msi
