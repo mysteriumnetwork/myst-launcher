878d7afb55a79a3836cd1e33904a128845b7d28f
Anton Litvinov <jnashicq@gmail.com>
2023-05-23 11:26:43 +0400

    Set version

--- cmd/resource/resource.go
+++ cmd/resource/resource.go
@@ -19,9 +19,9 @@ import (
 
 var (
 	// fixed non-string version. used for launcher version checks
-	intVersion = [4]uint16{1, 0, 41, 0}
+	intVersion = [4]uint16{1, 0, 42, 0}
 	// display version
-	strVersion = "1.0.41"
+	strVersion = "1.0.42"
 )
 
 func getIcon(path string) *winres.Icon {
